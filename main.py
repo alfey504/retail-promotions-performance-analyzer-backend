120722acd9f580eab9dc389d4e8e1f1784848d6e
alfey504 <alfey504@gmail.com>
2026-06-26 13:25:01 +0530

    added promotion fatigue

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from services.pipeline_services.pipe import pipe_data
 from services.db_services.session import init_db
 from services.kpi_services.incremental_uplift import get_incremental_sales_uplift
 from services.kpi_services.stockout_tracer import get_stockout_inventory_trace
-
+from services.kpi_services.post_promo_dip import get_post_promo_dip
+from services.kpi_services.redemption_demographics import get_redemption_demographics
 def main():
-    uplift = get_stockout_inventory_trace(2)
+    uplift = get_redemption_demographics(2)
     print(uplift)
 
-    for sku_invetory_trace in  uplift.sku_traces:
-        print(sku_invetory_trace)
+    # for sku_invetory_trace in  uplift.sku_traces:
+    #     print(sku_invetory_trace)
   
 
 
