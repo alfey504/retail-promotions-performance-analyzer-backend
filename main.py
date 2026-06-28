d0b2188951122c69915ad5e3191bfde5de5a0c1e
unknown <alfey504@gmail.com>
2026-06-28 12:47:06 +0530

    added passlib to dependency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from services.db_services.session import init_db
 from services.kpi_services.historic_kpi_calculators.historic_uplift import historic_uplift
+
 def main():
     init_db()
     uplifts = historic_uplift()
